3072b0db9097fdc42ccb4d06d4eb5874740dc3c4
Berat Bozkurt <beratbozkurt1999@gmail.com>
2021-10-23 12:47:24 +0300

    header custom hooks yazıldı

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,18 +1,24 @@
+import { Link } from "react-router-dom";
+
+import useHeader from "./hooks/useHeader";
+
 const Header: React.FC = () => {
-  return <header className="w-10/12  flex flex-row items-center justify-between m-auto text-gray-600 body-font">
-    <h1 className="text-4xl">Logo</h1>
-    <nav className="list-none flex flex-row gap-2">
-      <li className="list-none flex items-center">
-        Homepage
-      </li>
-      <li className="list-none flex items-center">
-        Profile
-      </li>
-      <li className="list-none flex items-center">
-        Logout
-      </li>
-    </nav>
-</header>
+  const { menu } = useHeader();
+
+  return (
+    <header className="w-10/12  flex flex-row items-center justify-between m-auto text-gray-600 body-font">
+      <h1 className="text-4xl text-gray-500 font-light">Logo</h1>
+      <nav className="list-none flex flex-row gap-2">
+        {menu.map((item) => (
+          <Link key={item.key} to={item.url}>
+            <li className="text-lg list-none flex items-center">
+              {item.title}
+            </li>
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
